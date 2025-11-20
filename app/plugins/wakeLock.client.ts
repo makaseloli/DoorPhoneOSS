59f9bf1c106ab120aac8f5b03fceace2f9967cea
seloli <114403771+makaseloli@users.noreply.github.com>
2025-11-20 14:36:07 +0900

    wakelockが働くように

--- app/plugins/wakeLock.client.ts
+++ app/plugins/wakeLock.client.ts
@@ -1,23 +1,25 @@
-let wakeLock: WakeLockSentinel | null = null;
+export default defineNuxtPlugin(() => {
+  let wakeLock: WakeLockSentinel | null = null;
 
-async function requestWakeLock() {
-  try {
-    wakeLock = await navigator.wakeLock.request('screen');
-    console.log('WakeLock有効');
-    wakeLock.addEventListener('release', () => {
-      console.log('WakeLock解除');
-    });
-  } catch (err) {
-    console.error('WakeLock取得失敗:', err);
+  async function requestWakeLock() {
+    try {
+      wakeLock = await navigator.wakeLock.request('screen');
+      console.log('WakeLock有効');
+      wakeLock.addEventListener('release', () => {
+        console.log('WakeLock解除');
+      });
+    } catch (err) {
+      console.error('WakeLock取得失敗:', err);
+    }
   }
-}
 
-document.addEventListener('visibilitychange', () => {
-  if (wakeLock !== null && document.visibilityState === 'visible') {
+  document.addEventListener('visibilitychange', () => {
+    if (wakeLock !== null && document.visibilityState === 'visible') {
+      requestWakeLock();
+    }
+  });
+
+  if ('wakeLock' in navigator) {
     requestWakeLock();
   }
 });
-
-if ('wakeLock' in navigator) {
-  requestWakeLock();
-}
